cf3003359fa2265d0239bbfb03be25efd1b7a718
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-10-19 16:07:05 +0000

    get_puuid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
@@ -8,4 +7,6 @@ headers = {
     "X-Riot-Token": "RGAPI-024957a5-1c00-463b-bf1e-7d15e10ee271"
 }
 
-def get_puuid(name, region)
+def get_puuid(name, region):
+  page = requests.get(f"https://{region.lower()}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}", headers=headers).json()
+  return page["puuid"]
