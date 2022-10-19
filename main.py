132375705ffaa1e07f86bd807f59b81c6b3d9f6a
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-10-19 16:26:28 +0000

    simple_search

--- main.py
+++ main.py
@@ -4,9 +4,36 @@ headers = {
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
     "Origin": "https://developer.riotgames.com",
+
+  
     "X-Riot-Token": "RGAPI-024957a5-1c00-463b-bf1e-7d15e10ee271"
 }
 
 def get_puuid(name, region):
   page = requests.get(f"https://{region.lower()}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}", headers=headers).json()
   return page["puuid"]
+
+def get_matches(puuid):
+  matches = []
+  i = 0
+  while(True):
+    page = requests.get(f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?&start={i}&count={100}", headers=headers)
+    
+    jp = page.json()
+    if len(jp) == 0:
+      break
+    matches += jp
+    i += 100
+  return matches
+
+def simple_search(name1, name2, region):
+  puuid1 = get_puuid(name1, region)
+  puuid2 = get_puuid(name2, region)
+  
+  matches1 = get_matches(puuid1)
+  matches2 = get_matches(puuid2)
+
+  return set(matches1) & set(matches2)
+  
+x = simple_search("PyMeirelles", "Grasolar", "BR1")
+print(x, len(x))
